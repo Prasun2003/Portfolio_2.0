dd0e6001be1789f399a203670f914fc01f250dca
PRASUN ROY <111894862+Prasun2003@users.noreply.github.com>
2023-09-04 16:36:52 +0530

    Add files via upload

---
+++ Portfolio 2.0/main.js
@@ -0,0 +1,7 @@
+var typed = new typed(".text", {
+    strings:["Frontend Developer", "Web Developer", "Blockchain"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
